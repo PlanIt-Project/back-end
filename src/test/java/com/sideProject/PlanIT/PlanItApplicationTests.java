758212b36d0938e1b1854de319e9075c66d5bba0
JuhyunPark1831 <epahs1831@naver.com>
2024-02-27 14:48:23 +0900

    FEAT: Initialize

---
+++ src/test/java/com/sideProject/PlanIT/PlanItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideProject.PlanIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
