716123db6da9a9e0ffe8babfb69221f19c3b44ea
moonjin-kim <ace39418@gmail.com>
2024-03-02 19:43:43 +0900

    feat: ApiResponse 추가, CustomErrorHandler 추가

---
+++ src/main/java/com/sideProject/PlanIT/common/response/CustomException.java
@@ -0,0 +1,18 @@
+package com.sideProject.PlanIT.common.response;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public CustomException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
---
+++ src/main/java/com/sideProject/PlanIT/common/response/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.sideProject.PlanIT.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    INVALID_PARAMETER(400, "파라미터 값을 확인해주세요.");
+
+
+    private final int status;
+    private final String message;
+}
