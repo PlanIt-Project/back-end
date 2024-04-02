4f2e441f8c687524441f78c7a5b0e11c976cd25c
moonjin-kim <ace39418@gmail.com>
2024-04-03 00:36:06 +0900

    fix: 프로그램 타입 추가 #138

--- src/main/java/com/sideProject/PlanIT/domain/post/dto/request/BannerRequestDto.java
+++ src/main/java/com/sideProject/PlanIT/domain/post/dto/request/BannerRequestDto.java
@@ -8,17 +8,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDate;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BannerRequestDto {
     private String title;
     private LocalDate startAt;
     private LocalDate endAt;
     private MultipartFile image;
 
-    public BannerRequestDto(String title, LocalDate startAt, LocalDate endAt, MultipartFile image) {
-        this.title = title;
-        this.startAt = startAt;
-        this.endAt = endAt;
-        this.image = image;
-    }
 }
